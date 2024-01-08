63c24f7cf671305542cbd011fbe794073129532b
Codidodido <139787880+Codidodido@users.noreply.github.com>
2024-01-08 09:56:46 +0330

    Add constructor to node class

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@ using namespace std;
 
 
 class Node {
+private:
     int data;
     Node* next;
 
     static Node* head;
+
+public:
+    Node(int _data, Node* _next = nullptr) {
+        this->data = _data;
+        this->next = _next;
+    }
 };
 
 
